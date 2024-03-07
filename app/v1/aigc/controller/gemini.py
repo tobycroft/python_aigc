ec2b0d078c814971a3cbd0ee02a4ec35b667f084
tuuz <silverhawk@foxmail.com>
2024-03-08 02:18:16 +0800

    `TuuzPC`

--- app/v1/aigc/controller/gemini.py
+++ app/v1/aigc/controller/gemini.py
@@ -22,17 +22,29 @@ def before():
     pass
 
 
+client = None
+chat = None
+
+
 @gemini.route('/text')
 async def text():
+    global client, chat
     token = tuuz.Input.Get.String("token")
     data = tuuz.Database.Db().table("ai_project").whereRow('token', token).find()
     gemini = tuuz.Database.Db().table("ai_gemini").whereRow('project_name', data["name"]).find()
     if gemini is None:
         return tuuz.Ret.fail(400, 'gemini未启用')
-
-    client = GeminiClient(gemini["Secure_1PSID"], gemini["Secure_1PSIDTS"], proxy=None)
-    await client.init(timeout=30, auto_close=False, close_delay=300)
-    chat = client.start_chat()
+    if chat is None:
+        if client is None:
+            client = GeminiClient(gemini["Secure_1PSID"], gemini["Secure_1PSIDTS"], proxy=None)
+            try:
+                await client.init(timeout=30, auto_close=False, close_delay=300)
+            except Exception as e:
+                return tuuz.Ret.fail(400, e)
+        chat = client.start_chat()
+    print(chat.rcid, chat.cid, chat.rid, chat.metadata)
     response = await chat.send_message("你好?")
+    print(response.candidates)
+    print(chat.rcid, chat.cid, chat.rid, chat.metadata)
     print(response)
     return tuuz.Ret.fail(400, response)
