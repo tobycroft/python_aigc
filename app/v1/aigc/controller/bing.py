ecbea774bd74232dcbfde35583187d0913544d18
tuuz <silverhawk@foxmail.com>
2024-03-11 15:23:14 +0800

    `TuuzPC`

--- app/v1/aigc/controller/bing.py
+++ app/v1/aigc/controller/bing.py
@@ -55,7 +55,7 @@ async def text():
         # If you are using non ascii char you need set ensure_ascii=False
         print(json.dumps(response, indent=2, ensure_ascii=False))
         conversation = await bot.chat_hub.get_conversation()
-        print(conversation)
+        # print(conversation)
         tuuz.Database.Db().table("ai_bing").whereRow('project_name', data["name"]).update({"conversation": json.dumps(conversation)})
         if response["messages_left"] < 1:
             await bot.close()
