5b8957abce52edc7c63cf1bfc7ea3d73335aab7e
tuuz <silverhawk@foxmail.com>
2024-03-12 16:01:07 +0800

    `TuuzPC`

--- app/v1/tts/controller/index.py
+++ app/v1/tts/controller/index.py
@@ -1,8 +1,6 @@
 import os
 
 from flask import Blueprint
-from paddlespeech.cli.tts.infer import TTSExecutor
-
 import tuuz.Ret
 
 Controller = Blueprint(os.path.splitext(os.path.basename(__file__))[0], __name__)
@@ -15,7 +13,4 @@ def slash():
 
 @Controller.post('/text')
 async def text():
-    # 加载tokenizer
-    tts = TTSExecutor()
-    tts(text="今天天气十分不错。", output="output.wav")
     return tuuz.Ret.success(0, )
