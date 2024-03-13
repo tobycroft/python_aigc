3c113d60189785b4224717aeb34c5a64350a326b
tuuz <silverhawk@foxmail.com>
2024-03-13 21:04:25 +0800

    `TuuzPC`

--- app/v1/aigc/controller/bing.py
+++ app/v1/aigc/controller/bing.py
@@ -66,8 +66,7 @@ async def text():
         response = await bot.ask(
             prompt=text,
             conversation_style=ConversationStyle.precise,
-            simplify_response=True,
-            search_result=False,
+            simplify_response=False,
         )
     except Exception as error:
         bot = None
@@ -75,36 +74,73 @@ async def text():
         return tuuz.Ret.fail(500, error, error)
 
     # If you are using non ascii char you need set ensure_ascii=False
-    print(json.dumps(response, indent=2, ensure_ascii=False))
+    # print(json.dumps(response, indent=2, ensure_ascii=False))
     conversation = await bot.chat_hub.get_conversation()
     # print(conversation)
     tuuz.Database.Db().table("ai_bing").whereRow('project_name', data["name"]).update({"conversation": json.dumps(conversation)})
-    if response["messages_left"] < 1:
-        await bot.close()
-        bot = None
-    try:
-        output_cleaned = re.sub(r'\[\^\d\^\]', '', response["text"])
-        json_str = re.search(r'{.*}', output_cleaned, re.DOTALL).group()
-        result = json.loads(json_str)
-        final_resp = ""
+    # if response["messages_left"] < 1:
+    #     await bot.close()
+    #     bot = None
+    # try:
+    messages = response["item"]["messages"]
+    output_cleaned = re.sub(r'\[\^\d\^]', '', response["item"]["result"]["message"])
+    final_resp = output_cleaned.replace("<br>", "\n")
 
-        # 去除原始内容中的JSON部分
-        output_without_json = re.sub(r'{.*}', '', output_cleaned, flags=re.DOTALL)
-        final_resp += output_without_json
-
-        for item in result['web_search_results']:
-            title = item['title']
-            max_length = 15
-            title = truncate_text(title, max_length)
-            url = item['url']
-
-            final_resp += "\n" + f"标题：{title}\nURL：{url}\n"
-            # print(f"标题：{title}\nURL：{url}\n")
-        final_resp = re.sub(r'\n', r'\r\n', final_resp)
-        final_resp = final_resp.replace("Generating answers for you...", "")
-        final_resp = re.sub(r'Searching the web for.*', '', final_resp)
-    except Exception as e:
-        final_resp = response["text"]
+    for item in messages:
+        if "sourceAttributions" in item:
+            for sourceAttributions in item['sourceAttributions']:
+                title = sourceAttributions['providerDisplayName']
+                max_length = 15
+                title = truncate_text(title, max_length)
+                url = sourceAttributions['seeMoreUrl']
+                final_resp += "\n" + f"标题：{title}\nURL：{url}\n"
+                print(f"标题：{title}\nURL：{url}\n")
+    final_resp = re.sub(r'\n', r'\r\n', final_resp)
+    #     final_resp = final_resp.replace("Generating answers for you...", "")
+    #     final_resp = re.sub(r'Searching the web for.*', '', final_resp)
+    # except Exception as e:
+    #     final_resp = response["text"]
     endtime = time.time()
     print("运行时间", endtime - starttime)
     return tuuz.Ret.success(0, response, final_resp)
+
+
+@Controller.post('/stream')
+async def stream():
+    token = tuuz.Input.Get.String("token")
+    text = tuuz.Input.Post.String("text")
+    data = tuuz.Database.Db().table("ai_project").whereRow('token', token).find()
+    bing = tuuz.Database.Db().table("ai_bing").whereRow('project_name', data["name"]).find()
+    if bing["cookies"] is None:
+        return tuuz.Ret.fail(400, 'bing未启用')
+    global bot
+    starttime = time.time()
+    if bot is None:
+        cookies = json.loads(bing["cookies"])
+        bot = await Chatbot.create(cookies=cookies)
+    try:
+        global conversation
+        conversation = json.loads(bing["conversation"])
+        print("设定conversation")
+        await bot.chat_hub.set_conversation(conversation_dict=conversation)
+    except Exception as error:
+        conversation = {}
+        bot = None
+        print(error)
+        return tuuz.Ret.fail(500, error, "conversation设定故障")
+
+    try:
+        async for final, response in bot.ask_stream(
+                prompt=text,
+                conversation_style=ConversationStyle.precise,
+                search_result=False,
+        ):
+            print(json.dumps(response, indent=2, ensure_ascii=False))
+    except Exception as error:
+        bot = None
+        print(error)
+        return tuuz.Ret.fail(500, error, error)
+
+    endtime = time.time()
+    print("运行时间", endtime - starttime)
+    return tuuz.Ret.success(0, )
