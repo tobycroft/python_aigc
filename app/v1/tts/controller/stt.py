8cc518fe6664f49e2cb4307d1175416ba030fcf0
tuuz <silverhawk@foxmail.com>
2024-03-16 01:09:52 +0800

    `TuuzPC`

--- app/v1/tts/controller/stt.py
+++ app/v1/tts/controller/stt.py
@@ -6,6 +6,7 @@ import ffmpeg
 import pysilk
 import requests
 from flask import Blueprint
+from urllib3.exceptions import InsecureRequestWarning
 
 import tuuz.Input
 import tuuz.Ret
@@ -60,6 +61,7 @@ async def audio():
 @Controller.post('/qq')
 async def qq():
     file = tuuz.Input.Post.String("file")
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
     response = requests.get(file, verify=False)
     response.raise_for_status()
     file_md5 = hashlib.md5()
