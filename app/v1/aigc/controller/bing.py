668829528a6386b914577c27f352654471384757
tuuz <silverhawk@foxmail.com>
2024-03-20 23:18:53 +0800

    `TuuzPC`

--- app/v1/aigc/controller/bing.py
+++ app/v1/aigc/controller/bing.py
@@ -114,7 +114,7 @@ async def text():
     endtime = time.time()
     print("运行时间", endtime - starttime)
     try:
-        tuuz.Database.Db().table("log").insert({"type": "bing","project": data["name"], "ask": text, "reply": json.dumps(response, indent=2, ensure_ascii=False)})
+        tuuz.Database.Db().table("log").insert({"type": "bing", "project": data["name"], "ask": text, "reply": json.dumps(response, indent=2, ensure_ascii=False)})
     except Exception as error:
         print("db-log", error)
     return tuuz.Ret.success(0, normal_text, final_resp)
