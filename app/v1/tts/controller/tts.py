ae0d8e3c467336d4247bc89f807e660cc1aeced2
tuuz <silverhawk@foxmail.com>
2024-03-20 23:43:40 +0800

    `TuuzPC`

--- app/v1/tts/controller/tts.py
+++ app/v1/tts/controller/tts.py
@@ -1,6 +1,10 @@
 import os
 
+import requests
 from flask import Blueprint
+from requests_toolbelt import MultipartEncoder
+
+import tuuz.Input
 import tuuz.Ret
 
 Controller = Blueprint(os.path.splitext(os.path.basename(__file__))[0], __name__)
@@ -13,4 +17,17 @@ def slash():
 
 @Controller.post('/text')
 async def text():
-    return tuuz.Ret.success(0, )
+    inp = tuuz.Input.Post.String("text")
+    params = {
+        "service": "StreamElements",
+        "voice": "Huihui",
+        "text": inp,
+    }
+
+    data = MultipartEncoder(fields=params)
+
+    headers = {
+        'Content-type': data.content_type
+    }
+    resp = requests.post("http://tts.aerofsx.com/request_tts.php", data=data, headers=headers)
+    return tuuz.Ret.success(0, resp, resp.text)
