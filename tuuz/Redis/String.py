a18017c93eb3d4db85cbd1d233fb15ce576aca25
tuuz <silverhawk@foxmail.com>
2024-03-26 00:08:30 +0800

    `TuuzPC`

--- tuuz/Redis/String.py
+++ tuuz/Redis/String.py
@@ -2,76 +2,76 @@ import config.app as app_conf
 from Redis.pyredis import RedisPy
 
 
-def string_set(key, value, exp=None):
+def Set(key, value, exp=None):
     if exp:
         RedisPy.setex(app_conf.Project + ":" + key, int(exp.total_seconds()), value)
     else:
         RedisPy.set(name=app_conf.Project + ":" + key, value=value)
 
 
-def string_get(key):
+def Get(key):
     return RedisPy.get(app_conf.Project + ":" + key)
 
 
-def string_getset(key, value):
+def Getset(key, value):
     return RedisPy.getset(app_conf.Project + ":" + key, value)
 
 
-def string_get_int(key):
+def Get_int(key):
     return int(RedisPy.get(app_conf.Project + ":" + key))
 
 
-def string_get_int64(key):
+def Get_int64(key):
     return int(RedisPy.get(app_conf.Project + ":" + key))
 
 
-def string_get_float64(key):
+def Get_float64(key):
     return float(RedisPy.get(app_conf.Project + ":" + key))
 
 
-def string_get_bytes(key):
+def Get_bytes(key):
     return RedisPy.get(app_conf.Project + ":" + key)
 
 
-def string_get_bool(key):
+def Get_bool(key):
     return bool(int(RedisPy.get(app_conf.Project + ":" + key)))
 
 
-def string_get_time(key):
+def Get_time(key):
     return RedisPy.get(app_conf.Project + ":" + key)
 
 
-def string_length(key):
+def Length(key):
     return RedisPy.strlen(app_conf.Project + ":" + key)
 
 
-def string_float64_incr(key, incr):
+def Float64_incr(key, incr):
     return RedisPy.incrbyfloat(app_conf.Project + ":" + key, incr)
 
 
-def string_int64_incr(key, incr):
+def Int64_incr(key, incr):
     return RedisPy.incrby(app_conf.Project + ":" + key, incr)
 
 
-def string_int64_decr(key, decr):
+def Int64_decr(key, decr):
     return RedisPy.decrby(app_conf.Project + ":" + key, decr)
 
 
-def delete(key):
+def Delete(key):
     return RedisPy.delete(app_conf.Project + ":" + key)
 
 
-def expire(key, duration):
+def Expire(key, duration):
     return RedisPy.expire(app_conf.Project + ":" + key, duration)
 
 
-def expire_time(key):
+def Expire_time(key):
     return RedisPy.ttl(app_conf.Project + ":" + key)
 
 
-def expire_at(key, expire_at):
+def Expire_at(key, expire_at):
     return RedisPy.expireat(app_conf.Project + ":" + key, expire_at)
 
 
-def check_exists(key):
+def Check_exists(key):
     return RedisPy.exists(app_conf.Project + ":" + key)
