d67127cc1f8bd5283782cc2de08202c7fba99d6e
tuuz <silverhawk@foxmail.com>
2024-03-26 16:23:45 +0800

    `TuuzPC`

---
+++ common/model/SystemParamModel.py
@@ -0,0 +1,11 @@
+from tuuz import Database
+
+Table = 'system_param'
+
+
+def Api_find(key):
+    return Database.Db().table(Table).where('key', key).find()
+
+
+def Api_find_value(key):
+    return Database.Db().table(Table).where('key', key).value('value')
