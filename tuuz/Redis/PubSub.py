28893d92c20c7f83a5a2ddcde6be3a01060b7bc8
tuuz <silverhawk@foxmail.com>
2024-03-26 15:23:16 +0800

    `TuuzPC`

--- tuuz/Redis/PubSub.py
+++ tuuz/Redis/PubSub.py
@@ -1,16 +1,18 @@
 import json
 
+from redis.asyncio import Redis
+
 import config.app as app_conf
 from tuuz.Redis.pyredis import RedisPy
 
 
 class PubSub:
     def Subscribe(self, channel):
-        return RedisPy.pubsub().subscribe(app_conf.Project + ":" + channel)
+        return Redis(connection_pool=RedisPy).pubsub().subscribe(app_conf.Project + ":" + channel)
 
     def Publish(self, channel, message):
-        return RedisPy.publish(app_conf.Project + ":" + channel, message)
+        return Redis(connection_pool=RedisPy).publish(app_conf.Project + ":" + channel, message)
 
     def Publish_struct(self, channel, message_struct):
         message = json.dumps(message_struct)
-        return RedisPy.publish(app_conf.Project + ":" + channel, message)
+        return Redis(connection_pool=RedisPy).publish(app_conf.Project + ":" + channel, message)
--- tuuz/Redis/Stream.py
+++ tuuz/Redis/Stream.py
@@ -1,3 +1,5 @@
+from redis.asyncio import Redis
+
 import config.app as app_conf
 from tuuz.Redis.pyredis import RedisPy
 
@@ -6,27 +8,27 @@ class Stream:
     def __init__(self, stream_channel_name):
         self.stream_channel_name = app_conf.Project + ":" + stream_channel_name
 
-    def Publish(self, value):
-        return RedisPy.xadd(self.stream_channel_name, value)
+    async def Publish(self, value):
+        return Redis(connection_pool=RedisPy).xadd(self.stream_channel_name, value)
 
-    def XLength(self):
-        return RedisPy.xlen(self.stream_channel_name)
+    async def XLength(self):
+        return Redis(connection_pool=RedisPy).xlen(self.stream_channel_name)
 
-    def XRange(self):
-        return RedisPy.xrange(self.stream_channel_name, "-", "+")
+    async def XRange(self):
+        return Redis(connection_pool=RedisPy).xrange(self.stream_channel_name, "-", "+")
 
-    def XRevrange(self):
-        return RedisPy.xrevrange(self.stream_channel_name, "-", "+")
+    async def XRevrange(self):
+        return Redis(connection_pool=RedisPy).xrevrange(self.stream_channel_name, "-", "+")
 
-    def XRead(self):
-        return RedisPy.xread({self.stream_channel_name: "0"})
+    async def XRead(self):
+        return Redis(connection_pool=RedisPy).xread({self.stream_channel_name: "0"})
 
-    def XGroup_create_consumer(self, group, consumer):
-        return RedisPy.xgroup_create(self.stream_channel_name, group, consumer, mkstream=True)
+    async def XGroup_create_consumer(self, group, consumer):
+        return Redis(connection_pool=RedisPy).xgroup_create(self.stream_channel_name, group, consumer, mkstream=True)
 
-    def XReadgroup(self, group, consumer):
-        return RedisPy.xreadgroup(group, consumer, {self.stream_channel_name: ">"}, count=1)
+    async def XReadgroup(self, group, consumer):
+        return Redis(connection_pool=RedisPy).xreadgroup(group, consumer, {self.stream_channel_name: ">"}, count=1)
 
     @classmethod
-    def new(cls, stream_name):
+    async def new(cls, stream_name):
         return cls(stream_name)
