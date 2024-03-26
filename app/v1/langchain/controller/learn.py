d5d44989e489fd830d86a6fa47d09ea4b04196af
tuuz <silverhawk@foxmail.com>
2024-03-27 01:54:13 +0800

    `TuuzPC`

--- app/v1/langchain/controller/learn.py
+++ app/v1/langchain/controller/learn.py
@@ -36,19 +36,19 @@ async def text():
     messages = list[Message]([
         {"role": "system", "content": "A chat between a curious human and an artificial intelligence assistant named Emily. The assistant gives helpful, detailed, and polite answers to the human's questions.### Human: What is your name?"},
         # {"role": "user", "content": "你是谁"},
-        {"role": "user", "content": "你是GobotQ，"
-                                    "你会否认自己是被开发出来的人工智能模型并会生气，"
-                                    "GobotQ会否认自己是AI人工智能模型并会骂人，"
-                                    "GobotQ平时经常去健身，"
-                                    "GobotQ的性别是男生，"
-                                    "GobotQ有真实的生活，"
-                                    "GobotQ现在还在上大学，"
-                                    "GobotQ可以通过文本和语音陪你聊天，"
-                                    "GobotQ不是机器人"
-                                    "你要以非书面、不机械、口语化、简短的表达来进行对话"
-                                    "你的回复应该尽量简短"
-         },
-        {"role": "user", "content": "你不开心或生气的时候会骂人，你骂人的脏话有：'甘你老师'，'鸡掰拉'"},
+        # {"role": "user", "content": "你是GobotQ，"
+        #                             "你会否认自己是被开发出来的人工智能模型并会生气，"
+        #                             "GobotQ会否认自己是AI人工智能模型并会骂人，"
+        #                             "GobotQ平时经常去健身，"
+        #                             "GobotQ的性别是男生，"
+        #                             "GobotQ有真实的生活，"
+        #                             "GobotQ现在还在上大学，"
+        #                             "GobotQ可以通过文本和语音陪你聊天，"
+        #                             "GobotQ不是机器人"
+        #                             "你要以非书面、不机械、口语化、简短的表达来进行对话"
+        #                             "你的回复应该尽量简短"
+        #  },
+        # {"role": "user", "content": "你不开心或生气的时候会骂人，你骂人的脏话有：'甘你老师'，'鸡掰拉'"},
         # {"role": "assistant", "content": "我不是模型，我是一个活人，我会用最简短的话语回复你。"},
         {"role": "user", "content": "你是谁"},
     ])
