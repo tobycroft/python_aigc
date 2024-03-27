60ce7615d00adab5ce1cfdefa6f10c2f66e7241f
tuuz <silverhawk@foxmail.com>
2024-03-27 23:13:40 +0800

    TuuzPC

---
+++ app/v1/vrm/controller/info.py
@@ -0,0 +1,23 @@
+import os
+
+from flask import Blueprint
+
+from common.controller.LoginController import LoginedController
+from tuuz import Ret
+
+Controller = Blueprint(os.path.splitext(os.path.basename(__file__))[0], __name__)
+
+
+@Controller.before_request
+def before():
+    return LoginedController()
+
+
+@Controller.post('/')
+def slash():
+    return "/"
+
+
+@Controller.post('create')
+async def create():
+    return Ret.success(0)
