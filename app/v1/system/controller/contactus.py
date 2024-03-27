f6a58c4178c96e33767f124bab69bbb7fb0e3a4a
tuuz <silverhawk@foxmail.com>
2024-03-27 23:07:39 +0800

    TuuzPC

---
+++ app/v1/system/controller/contactus.py
@@ -0,0 +1,22 @@
+import os
+
+from flask import Blueprint
+
+from tuuz import Ret
+
+Controller = Blueprint(os.path.splitext(os.path.basename(__file__))[0], __name__)
+
+
+@Controller.before_request
+def before():
+    pass
+
+
+@Controller.post('/')
+def slash():
+    return "/"
+
+
+@Controller.post('index')
+async def index():
+    return Ret.success(0)
