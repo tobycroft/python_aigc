00200d9ddee35bec256712cd15e29eec00a3fa40
tuuz <silverhawk@foxmail.com>
2024-03-30 02:18:07 +0800

    TuuzPC

--- app/v1/user/controller/info.py
+++ app/v1/user/controller/info.py
@@ -2,8 +2,10 @@ import os
 
 from flask import Blueprint
 
+from app.v1.user.model.UserInfoModel import UserInfoModel
 from common.controller.LoginController import LoginedController
 from tuuz import Ret
+from tuuz.Input import Header
 
 Controller = Blueprint(os.path.splitext(os.path.basename(__file__))[0], __name__)
 
@@ -18,6 +20,11 @@ def slash():
     return Controller.name
 
 
-@Controller.post('create')
-async def create():
-    return Ret.success(0)
+@Controller.post('get')
+async def get():
+    uid = Header.Int("uid")
+    ui = UserInfoModel().api_find_byUid(uid)
+    if ui is None:
+        UserInfoModel().api_insert(uid, "", "")
+        ui = UserInfoModel().api_find_byUid(uid)
+    return Ret.success(0, ui)
--- app/v1/user/model/UserInfoModel.py
+++ app/v1/user/model/UserInfoModel.py
@@ -1,1 +1,30 @@
+# CREATE TABLE `ai_user_info` (
+#   `id` int unsigned NOT NULL AUTO_INCREMENT,
+#   `uid` int unsigned DEFAULT '0',
+#   `group_name` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+#   `group_img` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+#   `date` datetime DEFAULT CURRENT_TIMESTAMP,
+#   PRIMARY KEY (`id`)
+# ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+from common.BaseModel import BaseModel
+from tuuz import Database
+
 Table = 'ai_user_info'
+
+
+class UserInfoModel(BaseModel):
+    Table = "ai_user"
+
+    def __init__(self, conn=None):
+        super().__init__(conn)
+
+    def api_find_byUid(self, uid):
+        return Database.Db(self.db).table(self.Table).where('uid', uid).find()
+
+    def api_insert(self, uid, group_name, group_img):
+        return Database.Db(self.db).table(self.Table).insert({
+            'uid': uid,
+            'group_name': group_name,
+            'group_img': group_img
+        })
