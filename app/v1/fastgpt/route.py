87f8935d86f73a57bc05e906d9a93560db1a7cb2
tuuz <silverhawk@foxmail.com>
2024-03-29 16:44:02 +0800

    TuuzPC

--- app/v1/fastgpt/route.py
+++ app/v1/fastgpt/route.py
@@ -13,10 +13,17 @@ def index():
     return folder_name
 
 
+# info
 import app.v1.fastgpt.controller.direct as info
 
 Route.register_blueprint(info.Controller, url_prefix=info.Controller.name)
 
+# embed
 import app.v1.fastgpt.controller.embed as embed
 
 Route.register_blueprint(embed.Controller, url_prefix=embed.Controller.name)
+
+# api
+import app.v1.fastgpt.controller.api as api
+
+Route.register_blueprint(api.Controller, url_prefix=api.Controller.name)
