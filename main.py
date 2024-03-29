966533420a9c8a80e1657c0813a4c08a62643045
tuuz <silverhawk@foxmail.com>
2024-03-29 19:13:38 +0800

    TuuzPC

--- main.py
+++ main.py
@@ -18,13 +18,20 @@ async def main():
     _ = create_task(tuuz.Redis.pyredis.init())
 
 
+app = MainRoute()
+
+
+@app.route('/')
+def index():
+    return 'AIGC'
+
+
 if __name__ == "__main__":
     run(main())
-    app = MainRoute()
     sch = BackgroundScheduler()
     sch.remove_all_jobs()
     sch.add_job(Token.refresh, 'interval', seconds=1)
     if not sch.running:
         sch.start()
     # app.run(host="0.0.0.0", port=84, debug=config.app.TestMode, threaded=True)
-    app.run(host="0.0.0.0", port=84, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=84, debug=True, threaded=True)
