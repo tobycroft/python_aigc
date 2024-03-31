3fb69a16b53e0d3250cde0501fca7279a3837196
tuuz <silverhawk@foxmail.com>
2024-03-31 19:20:43 +0800

    TuuzPC

---
+++ app/v1/live2d/model/Live2dTipsModel.py
@@ -0,0 +1,21 @@
+# CREATE TABLE `ai_live2d_tips` (
+#   `id` int unsigned NOT NULL AUTO_INCREMENT,
+#   `team_id` int unsigned DEFAULT '0',
+#   `type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '',
+#   `selector` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '',
+#   `texts` text COLLATE utf8mb4_general_ci,
+#   PRIMARY KEY (`id`)
+# ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+from common.BaseModel import BaseModel
+from tuuz import Database
+
+
+class Live2dTipsModel(BaseModel):
+    Table = "ai_live2d_tips"
+
+    def __init__(self, conn=None):
+        super().__init__(conn)
+
+    def api_select(self):
+        return Database.Db().table(self.Table).select()
