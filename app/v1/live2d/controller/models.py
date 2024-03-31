801d9fa8a0c267657d68aa8fdf916c2a077b50a9
tuuz <silverhawk@foxmail.com>
2024-03-31 17:53:03 +0800

    TuuzPC

--- app/v1/live2d/controller/models.py
+++ app/v1/live2d/controller/models.py
@@ -2,7 +2,9 @@ import os
 
 from flask import Blueprint
 
+from app.v1.live2d.model.Live2dModelModel import Live2dModelModel
 from tuuz import Ret
+from tuuz.Ret import success
 
 Controller = Blueprint(os.path.splitext(os.path.basename(__file__))[0], __name__)
 
@@ -15,3 +17,10 @@ def slash():
 @Controller.post('index')
 async def index():
     return Ret.success(0)
+
+
+# list
+@Controller.post('list')
+async def list():
+    datas = Live2dModelModel().api_select()
+    success(data=datas)
