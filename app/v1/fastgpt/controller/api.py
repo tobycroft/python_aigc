be7ee9041e387aca3f6b0896474db7064f80047e
tuuz <silverhawk@foxmail.com>
2024-04-01 13:34:48 +0800

    TuuzPC

--- app/v1/fastgpt/controller/api.py
+++ app/v1/fastgpt/controller/api.py
@@ -133,3 +133,56 @@ def raw():
     if len(ret.choices) > 0:
         ret_message = ret.choices[0].message.content
     return success(data=ret.model_dump(), echo=ret_message)
+
+
+@Controller.post('auto')
+def auto():
+    uid = Header.Int("uid")
+    chat_id = Post.Str("chat_id")
+    message = Post.Str("message")
+    subtoken = TeamSubtokenModel().api_find_byUidAndAmountOrIsLimit(uid, 5, 0, 0)
+    if not subtoken:
+        return Ret.fail(404, echo="没有找到对应的key")
+    if int(subtoken["is_limit"]) == 1 and float(subtoken["amount"]) <= 0:
+        return Ret.fail(403, echo="你的key已经没有余量了，请在控制台增加余量或将key设定为无限量模式")
+    if subtoken["coin_id"] != 5:
+        coin_name = ""
+        coin = CoinModel().api_find(subtoken["coin_id"])
+        if coin:
+            coin_name = coin["name"]
+        return Ret.fail(404, echo="key只能使用于" + coin_name)
+    fastgpt = FastgptModel().api_find_byId(subtoken["from_id"])
+    if not fastgpt:
+        return Ret.fail(404, echo="FastGPT中的上级Key被删除")
+    messages: list[dict] = []
+    records = FastgptRecordModel().api_find_bySubtokenIdAndChatId(subtoken["id"], chat_id)
+    if records:
+        messages += json.loads(records["send"])
+    messages.append({"role": "user", "content": message})
+    client = OpenAI(api_key=fastgpt["key"], base_url=fastgpt["base_url"])
+
+    ret = client.chat.completions.create(
+        model=fastgpt["model"],
+        messages=messages,
+        response_format={"type": "json_object"},
+        extra_body={
+            "chatId": chat_id,
+            "detail": fastgpt["detail"],
+        }
+        # temperature=0,
+    )
+    total_tokens = ret.usage.total_tokens
+    prompt_tokens = ret.usage.prompt_tokens
+    completion_tokens = ret.usage.completion_tokens
+
+    amount = CoinCalcAction(subtoken["coin_id"]).Calc(total_tokens)
+    TeamSubtokenModel().api_inc_amount_byKey(subtoken["key"], -abs(amount))
+
+    FastgptRecordModel().api_insert(fastgpt["id"], subtoken["id"], chat_id, json.dumps(messages, ensure_ascii=False), ret.model_dump_json(),
+                                    completion_tokens, prompt_tokens, total_tokens, "stop", amount)
+
+    # print(ret.model_dump(), total_tokens, prompt_tokens, completion_tokens)
+    ret_message = ""
+    if len(ret.choices) > 0:
+        ret_message = ret.choices[0].message.content
+    return success(data=messages, echo=ret_message)
