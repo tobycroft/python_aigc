8efc6c7297c922a4df46276f8c1a29b6306e7bed
tuuz <silverhawk@foxmail.com>
2024-04-03 19:02:25 +0800

    TuuzPC

--- app/v1/fastgpt/model/FastgptModel.py
+++ app/v1/fastgpt/model/FastgptModel.py
@@ -5,6 +5,8 @@
 #   `team_id` int DEFAULT NULL COMMENT 'team_id是添加到哪个队伍里面去的',
 #   `key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '',
 #   `base_url` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '访问的url，为空会跑到openai去',
+#   `model` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '' COMMENT '模型，可以留空',
+#   `detail` tinyint(1) DEFAULT '0',
 #   `change_date` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 #   `date` datetime DEFAULT CURRENT_TIMESTAMP,
 #   PRIMARY KEY (`id`)
@@ -31,3 +33,6 @@ class FastgptModel(BaseModel):
 
     def api_delete_byUidAndId(self, uid, id):
         return Database.Db().table(self.Table).whereRow("uid", uid).whereRow("id", id).delete()
+
+    def api_insert(self, uid, name, team_id, key, base_url, model, detail):
+        return Database.Db().table(self.Table).insert({"uid": uid, "name": name, "team_id": team_id, "key": key, "base_url": base_url, "model": model, "detail": detail})
