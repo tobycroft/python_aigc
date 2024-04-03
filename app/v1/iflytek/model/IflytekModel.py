056293dc200ea4592b1a1a039264a6256e58217e
tuuz <silverhawk@foxmail.com>
2024-04-04 00:13:41 +0800

    TuuzPC

--- app/v1/iflytek/model/IflytekModel.py
+++ app/v1/iflytek/model/IflytekModel.py
@@ -4,9 +4,9 @@
 #   `name` varchar(255) COLLATE utf8mb4_general_ci DEFAULT '',
 #   `team_id` int unsigned DEFAULT '0',
 #   `host` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT 'vms.cn-huadong-1.xf-yun.com',
-#   `app_secret` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+#   `api_secret` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
 #   `app_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-#   `app_key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+#   `api_key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
 #   `vcn` varchar(255) COLLATE utf8mb4_general_ci DEFAULT '',
 #   `change_date` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 #   `date` datetime DEFAULT CURRENT_TIMESTAMP,
@@ -26,13 +26,13 @@ class IflytekModel(BaseModel):
     def api_find_byId(self, id):
         return Database.Db().table(self.Table).where("id", id).find()
 
-    def api_insert(self, uid, name, team_id, host, app_secret, app_id, app_key, vcn):
+    def api_insert(self, uid, name, team_id, host, api_secret, app_id, api_key, vcn):
         return Database.Db().table(self.Table).insert(
-            {"uid": uid, "name": name, "team_id": team_id, "host": host, "app_secret": app_secret, "app_id": app_id, "app_key": app_key, "vcn": vcn})
+            {"uid": uid, "name": name, "team_id": team_id, "host": host, "api_secret": api_secret, "app_id": app_id, "api_key": api_key, "vcn": vcn})
 
-    def api_update_byUidAndId(self, uid, id, name, team_id, host, app_secret, app_id, app_key, vcn):
+    def api_update_byUidAndId(self, uid, id, name, team_id, host, api_secret, app_id, api_key, vcn):
         return Database.Db().table(self.Table).where("uid", uid).where("id", id).update(
-            {"name": name, "team_id": team_id, "host": host, "app_secret": app_secret, "app_id": app_id, "app_key": app_key, "vcn": vcn})
+            {"name": name, "team_id": team_id, "host": host, "api_secret": api_secret, "app_id": app_id, "api_key": api_key, "vcn": vcn})
 
     def api_delete(self, id):
         return Database.Db().table(self.Table).where("id", id).delete()
