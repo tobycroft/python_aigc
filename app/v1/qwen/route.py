b9ffbcce43ec535859c6563d8101829b15ff354e
tuuz <silverhawk@foxmail.com>
2024-04-03 23:00:10 +0800

    TuuzPC

---
+++ app/v1/qwen/route.py
@@ -0,0 +1,19 @@
+import os
+
+from flask import Blueprint
+
+script_path = os.path.abspath(__file__)
+folder_path = os.path.dirname(script_path)
+folder_name = os.path.basename(folder_path)
+Route = Blueprint(folder_name, __name__)
+
+
+@Route.route("/")
+def index():
+    return folder_name
+
+
+# info
+import app.v1.qwen.controller.info as info
+
+Route.register_blueprint(info.Controller, url_prefix=info.Controller.name)
