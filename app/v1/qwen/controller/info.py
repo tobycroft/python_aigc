56c8ec59b5262123c4a0f87cd9342d860a67f201
tuuz <silverhawk@foxmail.com>
2024-04-03 22:44:59 +0800

    TuuzPC

---
+++ app/v1/qwen/controller/info.py
@@ -0,0 +1,82 @@
+import os
+
+from flask import Blueprint
+
+from app.v1.fastgpt.model.FastgptModel import FastgptModel
+from common.controller.LoginController import LoginedController
+from tuuz import Ret
+from tuuz.Input import Header, Post
+
+Controller = Blueprint(os.path.splitext(os.path.basename(__file__))[0], __name__)
+
+
+@Controller.post('/')
+def slash():
+    return Controller.name
+
+
+@Controller.before_request
+def before_request():
+    return LoginedController()
+
+
+# list
+@Controller.post('/list')
+def list():
+    uid = Header.Int('uid')
+    data = FastgptModel().api_select_byUid(uid)
+    return Ret.success(data=data)
+
+
+# update
+@Controller.post('/update')
+def update():
+    uid = Header.Int('uid')
+    id = Post.Int('id')
+    name = Post.Str('name')
+    key = Post.Str('key')
+    base_url = Post.Str('base_url')
+    model = Post.Str('model')
+    detail = Post.Int('detail')
+    if FastgptModel().api_update_byUidAndId(uid, id, name, key, base_url, model, detail):
+        return Ret.success()
+    else:
+        return Ret.fail(500, echo='FastgptModel更新失败')
+
+
+# delete
+@Controller.post('/delete')
+def delete():
+    uid = Header.Int('uid')
+    id = Post.Int('id')
+    if FastgptModel().api_delete_byUidAndId(uid, id):
+        return Ret.success()
+    else:
+        return Ret.fail(500, echo='FastgptModel删除失败')
+
+
+# add
+@Controller.post('/add')
+def add():
+    uid = Header.Int('uid')
+    name = Post.Str('name')
+    team_id = Post.Int('team_id')
+    key = Post.Str('key')
+    base_url = Post.Str('base_url')
+    model = Post.Str('model')
+    detail = Post.Int('detail')
+    if FastgptModel().api_insert(uid, name, team_id, key, base_url, model, detail):
+        return Ret.success()
+    else:
+        return Ret.fail(500, echo='FastgptModel添加失败')
+
+
+# get
+@Controller.post('get')
+def get():
+    uid = Header.Int('uid')
+    id = Post.Int('id')
+    data = FastgptModel().api_find_byUidAndId(uid, id)
+    if data is None:
+        return Ret.fail(404, echo='数据不存在')
+    return Ret.success(data=data)
---
+++ app/v1/qwen/model/QianwenModel.py
@@ -0,0 +1,38 @@
+# CREATE TABLE `ai_qianwen` (
+#   `id` int unsigned NOT NULL AUTO_INCREMENT,
+#   `uid` int unsigned DEFAULT '0' COMMENT '添加人',
+#   `team_id` int DEFAULT NULL COMMENT 'team_id是添加到哪个队伍里面去的',
+#   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '' COMMENT 'key备注提示',
+#   `key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '',
+#   `rid` int DEFAULT '0',
+#   `model` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '' COMMENT '模型，可以留空',
+#   PRIMARY KEY (`id`)
+# ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+from common.BaseModel import BaseModel
+from tuuz import Database
+
+
+class FastgptModel(BaseModel):
+    Table = "ai_qianwen"
+
+    def __init__(self, conn=None):
+        super().__init__(conn)
+
+    def api_find_byId(self, id):
+        return Database.Db().table(self.Table).where("id", id).find()
+
+    def api_find_byUidAndId(self, uid, id):
+        return Database.Db().table(self.Table).where("uid", uid).where("id", id).find()
+
+    def api_select_byUid(self, uid):
+        return Database.Db().table(self.Table).where("uid", uid).select()
+
+    def api_update_byUidAndId(self, uid, id, name, key, model, detail):
+        return Database.Db().table(self.Table).where("uid", uid).where("id", id).update({"name": name, "key": key, "model": model, "detail": detail})
+
+    def api_delete_byUidAndId(self, uid, id):
+        return Database.Db().table(self.Table).where("uid", uid).where("id", id).delete()
+
+    def api_insert(self, uid, name, team_id, key, base_url, model, detail):
+        return Database.Db().table(self.Table).insert({"uid": uid, "name": name, "team_id": team_id, "key": key, "base_url": base_url, "model": model, "detail": detail})
