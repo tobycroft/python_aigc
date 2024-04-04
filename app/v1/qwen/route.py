184dde6f50283b1043fe46c47e9107b7cf491629
tuuz <silverhawk@foxmail.com>
2024-04-04 17:18:19 +0800

    TuuzPC

--- app/v1/qwen/route.py
+++ app/v1/qwen/route.py
@@ -17,3 +17,13 @@ def index():
 import app.v1.qwen.controller.info as info
 
 Route.register_blueprint(info.Controller, url_prefix=info.Controller.name)
+
+# direct
+import app.v1.qwen.controller.direct as direct
+
+Route.register_blueprint(direct.Controller, url_prefix=direct.Controller.name)
+
+# api
+import app.v1.qwen.controller.api as api
+
+Route.register_blueprint(api.Controller, url_prefix=api.Controller.name)
