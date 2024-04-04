628cb937ff543b2310c297763c99224d9ffc7c0f
tuuz <silverhawk@foxmail.com>
2024-04-04 13:56:53 +0800

    TuuzPC

--- app/v1/fastgpt/controller/direct.py
+++ app/v1/fastgpt/controller/direct.py
@@ -6,7 +6,6 @@ from flask import Blueprint
 from openai import OpenAI
 
 from app.v1.coin.action.CoinCalcAction import CoinCalcAction
-from app.v1.coin.model.CoinModel import CoinModel
 from app.v1.fastgpt.model.FastgptModel import FastgptModel
 from app.v1.fastgpt.model.FastgptRecordModel import FastgptRecordModel
 from app.v1.team.model.TeamSubtokenModel import TeamSubtokenModel
@@ -40,15 +39,11 @@ def text():
         return Ret.fail(404, echo="没有找到对应的key")
     if int(subtoken["is_limit"]) == 1 and float(subtoken["amount"]) <= 0:
         return Ret.fail(403, echo="你的key已经没有余量了，请在控制台增加余量或将key设定为无限量模式")
-    if subtoken["coin_id"] != 5:
-        coin_name = ""
-        coin = CoinModel().api_find(subtoken["coin_id"])
-        if coin:
-            coin_name = coin["name"]
-        return Ret.fail(404, echo="key只能使用于" + coin_name)
-    fastgpt = FastgptModel().api_find_byId(subtoken["from_id"])
+    team_id = subtoken["team_id"]
+    key = subtoken["key"]
+    fastgpt = FastgptModel().api_find_inTeamId([team_id])
     if not fastgpt:
-        return Ret.fail(404, echo="FastGPT中的上级Key被删除")
+        return Ret.fail(404, echo="没有找到对应的key")
     client = OpenAI(api_key=fastgpt["key"], base_url=fastgpt["base_url"])
     chat_id = flask.request.json.get("chatId")
     messages = flask.request.json.get("messages")
@@ -66,10 +61,10 @@ def text():
     prompt_tokens = ret.usage.prompt_tokens
     completion_tokens = ret.usage.completion_tokens
 
-    amount = CoinCalcAction(subtoken["coin_id"]).Calc(total_tokens)
-    TeamSubtokenModel().api_inc_amount_byKey(subtoken["key"], -abs(amount))
+    amount = CoinCalcAction("fastgpt").Calc(total_tokens)
+    TeamSubtokenModel().api_inc_amount_byKey(key, -abs(amount))
 
-    FastgptRecordModel().api_insert(fastgpt["id"], subtoken["id"], chat_id, json.dumps(messages, ensure_ascii=False), ret.model_dump_json(),
+    FastgptRecordModel().api_insert(fastgpt["id"], key, chat_id, json.dumps(messages, ensure_ascii=False), ret.model_dump_json(),
                                     completion_tokens, prompt_tokens, total_tokens, "stop", amount)
 
     # print(ret.model_dump(), total_tokens, prompt_tokens, completion_tokens)
