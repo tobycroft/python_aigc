bf2cbf825acb68f7ad37d07aaf13ad1003d111ad
tuuz <silverhawk@foxmail.com>
2024-04-04 14:00:54 +0800

    TuuzPC

--- app/v1/tts/controller/stt.py
+++ app/v1/tts/controller/stt.py
@@ -18,7 +18,7 @@ Controller = Blueprint(os.path.splitext(os.path.basename(__file__))[0], __name__
 @Controller.before_request
 def before():
     token = Input.Header.String("token")
-    data = Database.Db(self.db).table("ai_project").where('token', token).find()
+    data = Database.Db().table("ai_project").where('token', token).find()
     if data is None:
         return Ret.fail(400, 'project未启用')
     pass
