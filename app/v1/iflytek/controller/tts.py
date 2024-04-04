b8851aa872eac4392892a7870fe21bfa125dc94f
tuuz <silverhawk@foxmail.com>
2024-04-04 13:52:30 +0800

    TuuzPC

--- app/v1/iflytek/controller/tts.py
+++ app/v1/iflytek/controller/tts.py
@@ -4,7 +4,6 @@ from flask import Blueprint, Response
 
 from app.v1.iflytek.action.TtsAction import TtsAction
 from app.v1.iflytek.model.IflytekModel import IflytekModel
-from app.v1.team.model.TeamSubtokenModel import TeamSubtokenModel
 from app.v1.user.model.UserTeamModel import UserTeamModel
 from common.controller.LoginController import LoginedController
 from tuuz import Ret
@@ -23,39 +22,14 @@ def slash():
     return Controller.name
 
 
+@Controller.post('auto')
 @Controller.post('audio')
 async def audio():
     uid = Header.Int("uid")
     message = Post.Str("message")
-    subtoken = Post.Str("subtoken")
-    if not subtoken:
-        team_id = UserTeamModel().api_column_teamId_byUid(uid)
-        if not team_id:
-            return Ret.fail(404, echo="你还未加入任何团队")
-    else:
-        team_id = TeamSubtokenModel().api_value_teamId_bySubtoken(subtoken)
-        if not team_id:
-            return Ret.fail(404, echo="你还未加入任何团队")
-    iflytts = IflytekModel().api_find_inTeamId([team_id])
-    if not iflytts:
-        return Ret.fail(404, echo="没有找到对应的key")
-    b64 = TtsAction(iflytts["app_id"], iflytts["api_key"], iflytts["api_secret"], message, iflytts["vcn"]).data().get_audioBytes()
-    return Response(b64, mimetype="audio/mp3")
-
-
-@Controller.post('auto')
-async def auto():
-    uid = Header.Int("uid")
-    message = Post.Str("message")
-    subtoken = Post.Str("subtoken")
-    if not subtoken:
-        team_id = UserTeamModel().api_column_teamId_byUid(uid)
-        if not team_id:
-            return Ret.fail(404, echo="你还未加入任何团队")
-    else:
-        team_id = TeamSubtokenModel().api_value_teamId_bySubtoken(subtoken)
-        if not team_id:
-            return Ret.fail(404, echo="你还未加入任何团队")
+    team_id = UserTeamModel().api_column_teamId_byUid(uid)
+    if not team_id:
+        return Ret.fail(404, echo="你还未加入任何团队")
 
     iflytts = IflytekModel().api_find_inTeamId([team_id])
     if not iflytts:
