fd25ecdd355de4f1aef6149b3c891b2e810d4d30
tuuz <silverhawk@foxmail.com>
2024-04-07 02:19:54 +0800

    TuuzPC

--- app/v1/langchain/controller/milvus.py
+++ app/v1/langchain/controller/milvus.py
@@ -8,6 +8,11 @@ import tuuz.Ret
 
 Controller = Blueprint(os.path.splitext(os.path.basename(__file__))[0], __name__)
 
+from langchain.document_loaders import WebBaseLoader
+from langchain.embeddings import HuggingFaceBgeEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Milvus
+
 
 @Controller.post('/')
 def slash():
@@ -25,4 +30,34 @@ def before():
 
 @Controller.post('/text')
 async def text():
+    loader = WebBaseLoader("https://www.baidu.com")
+    # loader = TextLoader("story.txt")
+    documents = loader.load()
+    text_splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=0)
+    docs = text_splitter.split_documents(documents)
+
+    model_name = "/mnt/code/LLM_Service/text_model/bge-large-zh-v1.5/"
+    model_kwargs = {'device': 'cuda'}
+    encode_kwargs = {'normalize_embeddings': True}  # set True to compute cosine similarity
+    embeddings = HuggingFaceBgeEmbeddings(
+        model_name=model_name,
+        model_kwargs=model_kwargs,
+        encode_kwargs=encode_kwargs,
+        query_instruction=""
+    )
+
+    MILVUS_HOST = "192.168.175.4"
+    MILVUS_PORT = "19530"
+
+    vector_store = Milvus.from_documents(
+        docs,
+        embedding=embeddings,
+        collection_name="collection_1",
+        connection_args={"host": MILVUS_HOST, "port": MILVUS_PORT}
+    )
+
+    query = "托马斯是时间行者"
+    docs = vector_store.similarity_search_with_score(query)
+
+    print(docs)
     return tuuz.Ret.success(0, )
